195c70898de793fbf7cafe990dc44215eda3414e
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-22 11:14:17 +0300

    add the Navbar in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
+import Navbar from "./scenes/Navbar";
+import useMediaQuery from "./hooks/useMediaQuery";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div>
-      Hello
+    <div className="app bg-deep-blue">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
