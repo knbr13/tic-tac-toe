198f7d420414c45de0e904fd41981a0984a0f987
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-22 14:43:11 +0300

    1234567

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -70,7 +70,7 @@ const Contact = () => {
           <form
             target="_blank"
             onSubmit={onSubmit}
-            action="https://formsubmit.co/e8a5bdfa807605332f809e5656e27c6e"
+            action="https://formsubmit.co/abdullahalaadine63@gmail.com"
             method="POST"
           >
             <input
