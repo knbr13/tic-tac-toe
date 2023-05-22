3e8de76a9e5fea92bc741fa473af459b858cd1b3
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-22 14:34:24 +0300

    678

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
   return (
     <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
-        <h4 className="font-playfair text-3xl font-bold">JE</h4>
+        <h4 className="font-playfair text-3xl font-bold">Abdullah's Portfolio</h4>
         {isDesktop ? (
           <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
             <Link
