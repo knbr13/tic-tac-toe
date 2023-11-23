a1af5d07e4d79768a9f59debb0853c1c2ba59ca6
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-11-23 20:55:50 +0200

    create print_cell function

--- main.c
+++ main.c
@@ -6,6 +6,21 @@ const char board[3][3] = {
     {' ', ' ', ' '},
 };
 
+const char *print_cell(const char c)
+{
+    switch (c)
+    {
+    case 'X':
+        return " X ";
+        break;
+    case 'O':
+        return " O ";
+        break;
+    default:
+        return "   ";
+    }
+}
+
 int main()
 {
     printf("hello world\n");
