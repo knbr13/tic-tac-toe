aa37e47e1e505282cb1cee33b0184604cf6a01fa
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-11-23 21:02:31 +0200

    create print_board function

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+const char padding[5] = "    ";
+
 const char board[3][3] = {
     {' ', ' ', ' '},
     {' ', ' ', ' '},
@@ -18,6 +20,20 @@ const char *print_cell(const char c)
         break;
     default:
         return "   ";
+    }
+}
+
+void print_board()
+{
+    printf("\n%s+---+---+---+\n", padding);
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%s|", padding);
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%s|", print_cell(board[i][j]));
+        }
+        printf("\n%s+---+---+---+\n", padding);
     }
 }
 
