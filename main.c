425dfa40e5b8dc26f3c4b2c80e1d1efc8f59dde3
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-11-24 01:32:52 +0200

    create reset_game function

--- main.c
+++ main.c
@@ -117,6 +117,18 @@ void clear_input_buffer()
         ;
 }
 
+void reset_game()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            board[i][j] = ' ';
+        }
+    }
+    current_player = 'X';
+}
+
 int main()
 {
     while (1)
